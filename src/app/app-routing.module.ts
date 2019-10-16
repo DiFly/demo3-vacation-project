db506819fffa57d0b72f8fcdfee528639517bbd8
difly <kryshen.yaroslav@gmail.com>
2019-10-16 23:22:54 +0300

    create base structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'auth/login', pathMatch: 'full'},
+  {path: 'auth', loadChildren: './auth/auth.module#AuthModule'},
+  {path: 'user', loadChildren: './user/user.module#UserModule'},
+  {path: 'team-leader', loadChildren: './team-leader/team-leader.module#TeamLeaderModule'},
+  {path: 'admin', loadChildren: './admin/admin.module#AdminModule'},
+  {path: '**', redirectTo: '/auth/login', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,30 @@
+import {NgModule} from '@angular/core';
+import {RouterModule} from '@angular/router';
+import {CommonModule} from '@angular/common';
+import { AuthLayoutComponent } from './shared/components/auth-layout/auth-layout.component';
+import { LoginPageComponent } from './shared/components/login-page/login-page.component';
+import { ForgotPasswordPageComponent } from './shared/components/forgot-password-page/forgot-password-page.component';
+
+@NgModule({
+  declarations: [
+    AuthLayoutComponent,
+    LoginPageComponent,
+    ForgotPasswordPageComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild([
+      {
+        path: '', component: AuthLayoutComponent, children: [
+          {path: 'login', component: LoginPageComponent},
+          {path: 'forgot-password', component: ForgotPasswordPageComponent}
+        ]
+      }
+    ])
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AuthModule {
+}
