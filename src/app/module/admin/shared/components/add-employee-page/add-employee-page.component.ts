b7ec3976ecd59396f65d3d84cf56b3aef4874985
difly <kryshen.yaroslav@gmail.com>
2019-10-20 10:42:56 +0300

    Add add-new-employee page template

--- src/app/module/admin/shared/components/add-employee-page/add-employee-page.component.ts
+++ src/app/module/admin/shared/components/add-employee-page/add-employee-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-add-employee-page',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-employee-page.component.scss']
 })
 export class AddEmployeePageComponent implements OnInit {
+  editForm: FormGroup;
 
   constructor() { }
 
   ngOnInit() {
+    this.editForm = new FormGroup({
+      vacationType: new FormControl(),
+      // vacationDataFrom: new FormControl(),
+      // vacationDataTo: new FormControl()
+    });
+    this.editForm.controls.vacationType.setValue('0', {onlySelf: true});
   }
 
+  cancel() {
+  }
+
+  saveNewUser() {
+  }
 }
