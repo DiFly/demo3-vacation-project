c36fc2a8b95274e95cb62232af477cf971f8fcd2
yaroslav.kryshen <kryshen.yaroslav@gmail.com>
2019-10-22 14:38:50 +0300

    create shared.module, add model user, create file test-data.json

---
+++ src/app/shared/models/user-model.ts
@@ -0,0 +1,13 @@
+export class User {
+  id: string;
+  firstname: string;
+  surname: string;
+  userpic?: string;
+  emailself: string;
+  emailwork: string;
+  phone: string;
+  skype: string;
+  datestart: Date;
+  dateend?: Date;
+  team?: any;
+}
