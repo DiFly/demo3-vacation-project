315df2907afff03397ca832bbe2fc2884ca65870
difly <kryshen.yaroslav@gmail.com>
2019-11-05 17:31:11 +0200

    simple changes in auth service

--- src/app/module/auth/service/auth.service.ts
+++ src/app/module/auth/service/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -7,4 +8,20 @@ import {HttpClient} from '@angular/common/http';
 export class AuthService {
 
   constructor(private http: HttpClient) { }
+
+  SingIn(userDetails: {email: string, password: string}) {
+    const formData = new FormData();
+
+    formData.append('username', userDetails.email);
+    formData.append('password', userDetails.password);
+    formData.append('client_id', 'api');
+    formData.append('client_secret', 'vacationsecrets');
+    formData.append('grant_type', 'password');
+
+    return this.http.post('https://vacations.polytech.rocks:52538/connect/token', formData)
+      .pipe(map((response) => {
+        console.log('SingIn(): ', response);
+        sessionStorage.setItem('auth_token', response['access_token']);
+      }));
+  }
 }
